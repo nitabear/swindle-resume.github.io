8002d32d360b12e7229ca06493bb1b04becce2ac
Jason Swindle <jason.swindle@rackspace.com>
2014-08-22 09:58:13 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+var Handlebars = require("handlebars");
+
+function render(resume) {
+	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+	return Handlebars.compile(template)({
+		resume: resume
+	});
+}
+
+module.exports = {
+	render: render
+};
